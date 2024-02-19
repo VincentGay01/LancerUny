9c9afac1f1e4db481134faabce6b0947800f0372
VincentGay01 <101732915+VincentGay01@users.noreply.github.com>
2024-02-19 14:33:33 +0100

    modif dicerolls

--- Assets/DiceRolls.cs
+++ Assets/DiceRolls.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class DiceRolls 
 {
-    public int number;
-    public int typeDice;
-    public int modificator;
+    private int number;
+    private int typeDice;
+    private int modificator;
+    
 
     //rajouter les tags
 
@@ -29,5 +30,21 @@ public class DiceRolls
 
     public int getNumber() { return this.number; }
     public int gettypeDice() { return this.typeDice; }
+    public int getmodificator() { return this.modificator; }
 
+    public void setNumber(int number) { this.number = number;}
+
+    public void settypeDice(int typeDice) { this.typeDice = typeDice;}
+
+    public void setModificator(int modificator) { this.modificator = modificator;}  
+
+    public int roll() {
+        int res=0;
+        for (int i=0;i<this.number; i++)
+        {
+            res +=Random.Range(1,this.typeDice);
+        }
+        res += this.modificator;
+        return res;
+    }
 }
