62dd1c4ff19a81c478b20820aa9c5e1737c121e7
VincentGay01 <101732915+VincentGay01@users.noreply.github.com>
2024-02-19 15:50:00 +0100

    diceRoll done ,modif weapon
    finished diceRoll , modified Mechweapon

--- Assets/DiceRolls.cs
+++ Assets/DiceRolls.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -27,7 +28,7 @@ public class DiceRolls
     {
         this.modificator = modificator;
     }
-
+    // getter and setter
     public int getNumber() { return this.number; }
     public int gettypeDice() { return this.typeDice; }
     public int getmodificator() { return this.modificator; }
@@ -37,14 +38,45 @@ public class DiceRolls
     public void settypeDice(int typeDice) { this.typeDice = typeDice;}
 
     public void setModificator(int modificator) { this.modificator = modificator;}  
-
+    // function to roll a dice
     public int roll() {
         int res=0;
         for (int i=0;i<this.number; i++)
         {
-            res +=Random.Range(1,this.typeDice);
+            res += UnityEngine.Random.Range(1,this.typeDice);
         }
         res += this.modificator;
         return res;
+    }
+    //function to add two dice roll
+    public int addRoll(DiceRolls roll)
+    {
+        int res=0;
+        res += this.roll();
+        res += roll.roll();
+        return res;
+    }
+    // function to substract two dice roll
+    public int minusRoll(DiceRolls roll)
+    {
+        int res=0;
+        res += this.roll();
+        if((res-roll.roll())<1)
+        {
+            res = 1;
+        }
+        else
+        {
+            res -= roll.roll();
+        }
+        return res;
+    }
+    // divide a roll by 2 
+    public int divideDiceRoll()
+    {
+        double res=0;
+        res += this.roll();
+        res = Math.Ceiling(res/2);
+        return Convert.ToInt32(res);
     }
 }
--- Assets/MechWeapon.cs
+++ Assets/MechWeapon.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class MechWeapon 
 {
-    string name;
-    int range;
-    string typeDamage;
-    int damage;
-    int blast;
+   private string name;
+   private  int range;
+   private string typeDamage;
+   private  DiceRolls damage;
+   private  int blast;
+   private  string compendium;
+    //don't forget to add tag
+
+    public MechWeapon()
+    {
+        this.name = string.Empty;
+        this.range = 0;
+        this.typeDamage = string.Empty;
+        this.damage = new DiceRolls();
+        this.blast = 0;
+        this.compendium = string.Empty;
+
+    }
+
+    public MechWeapon(string name, int range, string typeDamage, DiceRolls damage, int blast, string compendium)
+    {
+        this.name = name;
+        this.range = range;
+        this.typeDamage = typeDamage;
+        this.damage = damage;
+        this.blast = blast;
+        this.compendium = compendium;
+    }
+    // getter and setter 
+    public string getName() { return name; }
+    public int getRange() { return range; } 
+    public string getTypeDamage() { return typeDamage;}
+    public DiceRolls getDamage() {  return damage;}
+    public int getBlast() {  return blast;}
+    public string getCompendium() {  return compendium;}
+
+    public void setName(string name) { this.name = name;}
+    public void setRange(int range) { this.range = range; }
+    public void setTypeDamage(string typeDamage) {  this.typeDamage = typeDamage;}
+    public void setDamage(DiceRolls damage) { this.damage = damage; }
+    public void setBlast(int blast) { this.blast = blast; }
+    public void setCompendium(string text) { this.compendium= text;}
 
 }
